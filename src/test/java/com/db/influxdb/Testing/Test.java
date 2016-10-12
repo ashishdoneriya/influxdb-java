d057fe80070a13e0fd3e8380c41c26cb4946a649
Ashish Doneriya <ashishdoneriya@gmail.com>
2016-10-12 12:27:05 +0530

    Update Test class

--- src/test/java/com/db/influxdb/Testing/Test.java
+++ src/test/java/com/db/influxdb/Testing/Test.java
@@ -21,13 +21,13 @@ public class Test {
 		DataWriter writer = new DataWriter(configuration);
 		writer.setTableName("sampleTable1");
 		writer.setTimeUnit(TimeUnit.SECONDS);
-		
+
 		writer.addField("column1", 12212);
 		writer.addField("column2", 22.44);
 		writer.addField("column3", "thisIsString");
 		writer.addField("column4", false);
 		writer.addTag("hostname", "server001");
-		
+
 		// If we'll set time it will set automatically
 		writer.setTime(System.currentTimeMillis() / 1000);
 		writer.writeData();
@@ -36,10 +36,11 @@ public class Test {
 		writer.addField("column2", 21.44);
 		writer.addField("column3", "thisIsString1");
 		writer.addField("column4", true);
+		// Influxdb saves one point at one time. To add another point at same
+		// time we can use tags otherwise it will override the previous point.
 		writer.addTag("disk_type", "HDD");
+		writer.setTime(System.currentTimeMillis() / 1000);
 		
-		// Influxdb saves one point at one time. Therefore we have to add another point at another time.
-		writer.setTime(System.currentTimeMillis() / 1000 + 1);
 		writer.writeData();
 
 	}
@@ -66,7 +67,7 @@ public class Test {
 
 		ResultSet resultSet = dataReader.getResult();
 		System.out.println(resultSet);
-		
+
 		Query query1 = new Query();
 		query1.setCustomQuery("select * from sampleTable1");
 		dataReader.setQuery(query1);
